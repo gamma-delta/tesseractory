9ab4d62b5d6efb9544812e3ab563e7f28e00f181
gamma-delta <petrathekat@gmail.com>
2023-09-16 10:13:10 -0500

    right i need to add git

--- godot/default.gd
+++ godot/default.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
-@onready
-var tesser : TesseractoryWorldHandler = $TesseractoryWorldHandler
-
-@onready
-var screen : TextureRect = $TextureRect
+@onready var tesser : TesseractoryWorldHandler = $TesseractoryWorldHandler
+@onready var screen : TextureRect = $TextureRect
+@onready var label : Label = $TextureRect/Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
   self.screen.texture = self.tesser.get_canvas_tex()
+
+func _process(_delta: float):
+  self.label.text = self.tesser.debug_string()
