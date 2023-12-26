0e7a4d3be3cbb1a84abca7d66901e326350937da
gamma-delta <petrathekat@gmail.com>
2023-12-25 23:47:41 -0600

    on to godot 4.2. moving more logic to godot

--- godot/default.gd
+++ godot/default.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Node
 
 @onready var tesser : TesseractoryGodotBridge = %TesseractoryGodotBridge
 @onready var viewport : SubViewport = %ScreenViewport
@@ -27,7 +27,7 @@ func _input(event: InputEvent):
     get_tree().quit()
   
   if event.is_action_pressed("pause"):
-    var paused  := get_tree().paused
+    var paused := get_tree().paused
     if paused:
       # Unpause
       pause_ui.hide()
